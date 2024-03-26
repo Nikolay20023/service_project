06ee486799cc5ae1d564ab579af7fc3ca00e5293
nikolay20023 <sudakovkoli@mail.ru>
2024-03-26 15:06:22 +0300

    front initial

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
-from core.logger import LOGGING
 from api.v1 import users, services
 from core.config import app_setting
 
@@ -10,6 +10,19 @@ app = FastAPI(
     docs_url="/api/openapi",
     openapi_url="/api/openapi.json",
     default_response_class=ORJSONResponse
+)
+
+origin = [
+    "http://localhost",
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origin,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PATCH", "DELETE"],
+    allow_headers=["Authorization", "Content-Type"],
 )
 
 app.include_router(users.router, prefix="/api/v1", tags=["users"])
